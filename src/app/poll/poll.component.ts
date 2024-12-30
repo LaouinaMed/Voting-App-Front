6bc6e04a316834f2a875c6cda67de7108f45be10
Mohammed Laouina <simed.laouina@gmail.com>
2024-12-30 16:05:14 +0100

    create service

---
+++ src/app/poll/poll.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-poll',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './poll.component.html',
+  styleUrls: ['./poll.component.css']
+})
+export class PollComponent {
+
+}
